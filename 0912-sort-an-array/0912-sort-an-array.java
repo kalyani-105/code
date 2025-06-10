a2e405c52d1454c00aaf25ff91ed1f508da4b92e
kalyani-105 <rkalyani2582005@gmail.com>
2025-06-10 15:41:12 +0530

    Time: 279 ms (14.18%), Space: 57.9 MB (10.47%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        quick(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    static void quick(int[] nums, int low, int high) {
+        if (low >= high) return;
+
+        int s = low;
+        int e = high;
+        int mid = (s + e) / 2;
+        int pivot = nums[mid]; 
+        while (s <= e) {
+            while (nums[s] < pivot) {
+                s++;
+            }
+            while (nums[e] > pivot) {
+                e--;
+            }
+
+            if (s <= e) {
+                // Swap elements on the wrong side
+                int temp = nums[s];
+                nums[s] = nums[e];
+                nums[e] = temp;
+                s++;
+                e--;
+            }
+        }
+
+        // Recursively sort the two halves
+        quick(nums, low, e);
+        quick(nums, s, high);
+    }
+}
